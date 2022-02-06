9788cc8181edc10eea421cfba05614ddf9d84a5a
jtsang4 <wtzeng1@gmail.com>
2022-02-07 01:29:26 +0800

    feat: change curly lint rule

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -110,7 +110,7 @@ module.exports = {
 
     // Common
     'semi': ['error', 'never'],
-    'curly': ['error', 'multi-or-nest', 'consistent'],
+    'curly': ['error', 'all'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent-as-needed'],
     'no-unused-vars': 'warn',
